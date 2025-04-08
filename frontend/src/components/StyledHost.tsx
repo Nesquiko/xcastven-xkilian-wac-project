b780d3e671697b2763314433df8f214c9c3ebf8e
Nesquiko <lukas.castven@gmail.com>
2025-04-08 12:16:24 +0200

    fetch google fonts and icons

--- frontend/src/components/StyledHost.tsx
+++ frontend/src/components/StyledHost.tsx
@@ -4,6 +4,9 @@ export const StyledHost: typeof Host = (attrs, children) => {
   return (
     <Host {...attrs}>
       <link rel="stylesheet" href={getAssetPath('xcastven-xkilian-project.css')} />
+      <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" rel="stylesheet" />
+      <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
       {children}
     </Host>
   );
